e86df33adc98106c3dc2c7f8365c499771bc1a51
Jkaunistunturi <Jenny.Kaunistunturi@edunord.se>
2024-08-06 13:19:02 +0300

    test commit

--- Monday test 1/obj/Debug/net8.0/Monday test 1.AssemblyInfo.cs
+++ Monday test 1/obj/Debug/net8.0/Monday test 1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Monday test 1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abb84213d912e31c1aa5b8bd76632be7610d68a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Monday test 1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Monday test 1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
